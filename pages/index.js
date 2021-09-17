56c99a2b62bcd190a13133d12bd5d001d3e0d94f
darshjain <darshj786@gmail.com>
2021-09-17 19:39:18 +0530

    List of items cards in index

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import Layout from "../components/Layout";
+import {
+	Button,
+	Card,
+	Grid,
+	CardActionArea,
+	CardMedia,
+	CardContent,
+	Typography,
+	CardActions,
+} from "@material-ui/core";
+import data from "../utils/data";
 
 export default function Home() {
-  return (
-    <Layout>
-      <div>
-        <h1>Products</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-        </ul>
-      </div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<div>
+				<h1>Products</h1>
+				<Grid container spacing={3}>
+					{data.products.map((product) => (
+						<Grid item md={4} key={product.name}>
+							<Card>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										image={product.image}
+										title={product.name}
+									></CardMedia>
+									<CardContent>
+										<Typography>{product.name}</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Typography>${product.price}</Typography>
+									<Button size="small" color="primary-color">
+										Add to Cart
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</div>
+		</Layout>
+	);
 }
