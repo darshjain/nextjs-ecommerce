c328de78566d09bdb91a203cab217b09981d60da
darshjain <darshj786@gmail.com>
2021-09-17 17:48:30 +0530

    Basic Layout and Material UI FIX

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import React, { Children } from 'react'
+import Head from 'next/head'
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import useStyles from '../utils/styles'
+
+export default function Layout({children}) {
+    const classes =useStyles()
+    return (
+        <div>
+            <Head>
+                <title>Next Amazona</title>
+            </Head>
+            <AppBar position = "static" className={classes.navbar}>
+                <Toolbar>
+                    <Typography>Amazona</Typography>
+                </Toolbar>
+            </AppBar>
+            <Container className={classes.main}>
+                {children}
+            </Container>
+            <footer className={classes.footer}>
+                <Typography>All Rights Reserved. Next Amazona</Typography>
+            </footer>
+        </div>
+    )
+}
